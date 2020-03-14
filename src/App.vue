674809ae4e38fbe4e035c33217d48b2965136df3
coffeeteer <tgrieshiemer@gmail.com>
2020-03-13 20:59:30 -0400

    project complete

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,101 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Http</h1>
-            </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Http</h1>
+        <div class="form-group">
+          <label>Username</label>
+          <input type="text" class="form-control" v-model="user.username">
         </div>
+        <div class="form-group">
+          <label>Email</label>
+          <input type="text" class="form-control" v-model="user.email">
+        </div>
+        <button class="btn btn-primary" @click="submit">Submit</button>
+        <hr>
+        <input type="text" class="form-control" v-mdoel="node">
+        <hr>
+        <br>
+        <br>
+        <button class="btn btn-primary" @click="fetchData">Fetch</button>
+        <br>
+        <br>
+        <ul class="list-group">
+          <li
+            class="list-group-item"
+            v-for="u in users"
+            :key="u.id"
+          >{{ u.username }} - {{ u.email }}</li>
+        </ul>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
+export default {
+  data: function() {
+    return {
+      user: {
+        username: "",
+        email: ""
+      },
+      users: [],
+      resource: {},
+      node: "data"
+    };
+  },
+  methods: {
+    submit() {
+      // this.$http
+      //   .post("data.json", this.user)
+      //   .then(
+      //     response => {
+      //       console.log(response);
+      //     },
+      //     error => {
+      //       console.log(error);
+      //     }
+      //   )
+      //   .then(() => (this.user = ""));
+      // this.resource.save({}, this.user);
+      this.resource.saveAlt(this.user);
+    },
+    fetchData() {
+      // this.$http
+      //   .get("data.json")
+      // .then(response => {
+      //   return response.json();
+      // })
+      // .then(data => {
+      //   const resultArray = [];
+      //   for (let key in data) {
+      //     resultArray.push(data[key]);
+      //   }
+      //   this.users = resultArray;
+      // });
+      this.resource
+        .getData({ node: this.node })
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          const resultArray = [];
+          for (let key in data) {
+            resultArray.push(data[key]);
+          }
+          this.users = resultArray;
+        });
     }
+  },
+  created() {
+    const customAction = {
+      saveAlt: { method: "POST", url: "alernative.json" },
+      getData: { method: "GET" }
+    };
+    this.resource = this.$resource("{node}.json", {}, customAction);
+  }
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import VueResource from "vue-resource";
+import App from "./App.vue";
+
+Vue.use(VueResource);
+
+Vue.http.options.root = "https://http-test-projec.firebaseio.com/";
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  if (request.method == "POST") {
+    request.method = "PUT";
+  }
+  next(response => {
+    response.json = () => {
+      return { messages: response.body };
+    };
+  });
+});
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});
